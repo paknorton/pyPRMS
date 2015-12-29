05c893454bc90128ea2c29fb55787aaa012d6224
Parker Norton <parker.norton@gmail.com>
2015-12-29 07:36:29 -0700

    misc updates

--- prms_lib.py
+++ prms_lib.py
@@ -15,6 +15,12 @@ import sys
 #                  statvar - processes the output statvar file
 #                  control - processes the control file
 
+# Although this software program has been used by the U.S. Geological Survey (USGS),
+# no warranty, expressed or implied, is made by the USGS or the U.S. Government as
+# to the accuracy and functioning of the program and related program material nor
+# shall the fact of distribution constitute any such warranty, and no
+# responsibility is assumed by the USGS in connection therewith.
+
 __version__ = '0.5'
 
 
