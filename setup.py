2027c66b9eeb9d1416347fcc0379220238d06687
Parker A. Norton <pnorton@usgs.gov>
2017-06-22 10:23:33 -0600

    Working the package setup

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'pandas', 'future'],
+    install_requires=['setuptools'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
