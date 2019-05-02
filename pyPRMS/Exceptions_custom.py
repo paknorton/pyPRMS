4f111957511f99ffb2143e2ac8026e3cf8c92466
Parker A. Norton <pnorton@usgs.gov>
2019-05-02 11:15:41 -0600

    Add ConcatError class

--- pyPRMS/Exceptions_custom.py
+++ pyPRMS/Exceptions_custom.py
@@ -2,10 +2,16 @@
 from __future__ import (absolute_import, division, print_function)
 
 
+class ConcatError(Exception):
+    """Concatenation error"""
+    pass
+
+
 class ParameterError(Exception):
     def __init__(self, err_args):
         Exception.__init__(self, err_args)
         self.errArgs = err_args
+
 
 class ControlError(Exception):
     def __init__(self, err_args):
