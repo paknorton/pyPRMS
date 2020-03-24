069ea794beb1b03ddf179c68a909b036156c92a5
Parker A. Norton <pnorton@usgs.gov>
2020-03-24 16:20:56 -0600

    Cleanup imports

--- pyPRMS/Parameters.py
+++ pyPRMS/Parameters.py
@@ -1,16 +1,10 @@
 
-# from __future__ import (absolute_import, division, print_function)
-# from future.utils import iteritems
-
 import numpy as np
 import pandas as pd
 from collections import OrderedDict
-# import xml.etree.ElementTree as xmlET
 
 from pyPRMS.Parameter import Parameter
 from pyPRMS.Exceptions_custom import ParameterError
-# from pyPRMS.constants import DATA_TYPES
-# from pyPRMS.Dimensions import ParamDimensions
 
 
 class Parameters(object):
