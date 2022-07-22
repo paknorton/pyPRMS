daa8107e30a2f8fcf7d18daf8c76b28cdf828141
Parker Norton <pnorton@usgs.gov>
2022-07-22 12:09:54 -0600

    Updates to various functions
    - type hinting updates
    - read_netcdf() now returns an xarray dataset
    - read_netcdf() defaults to entire date range from source netcdf file when start and end date arguments are None
    - write_ascii() now writes a single variable to a file instead of handling lists of variables for writing
    - write_netcdf() changed how time data is selected from source netcdf (was .loc now .sel)

--- pyPRMS/CbhNetcdf.py
+++ pyPRMS/CbhNetcdf.py
@@ -1,9 +1,10 @@
 import datetime
-import pandas as pd
-import netCDF4 as nc
+import numpy as np
+import pandas as pd   # type: ignore
+import netCDF4 as nc   # type: ignore
 import xarray as xr
 
-from typing import List, Optional
+from typing import List, Optional, Union
 
 # from typing import Any,  Union, Dict, List, OrderedDict as OrderedDictType
 
@@ -33,68 +34,56 @@ class CbhNetcdf(object):
         self.__endate = en_date
         self.__nhm_hrus = nhm_hrus
         self.__date_range = None
-        self.__dataset = None
+        # self.__dataset = None
         self.__final_outorder = None
         self.__thredds = thredds
 
-        self.read_netcdf()
+        self.__dataset = self.read_netcdf()
 
-    def read_netcdf(self):
+    def read_netcdf(self) -> xr.Dataset:
         """Read CBH files stored in netCDF format.
         """
 
-        if self.__nhm_hrus:
-            # print('\t\tOpen dataset')
-            if self.__thredds:
-                # thredds_server = 'http://gdp-netcdfdev.cr.usgs.gov:8080'
-                thredds_server = 'http://localhost:8080'
-                base_opendap = f'{thredds_server}/thredds/dodsC/NHM_CBH_GM/files'
+        if self.__thredds:
+            # thredds_server = 'http://gdp-netcdfdev.cr.usgs.gov:8080'
+            thredds_server = 'http://localhost:8080'
+            base_opendap = f'{thredds_server}/thredds/dodsC/NHM_CBH_GM/files'
 
-                # base_url is used to get a list of files for a product
-                # Until xarray supports ncml parsing the list of files will have to be manually built
-                base_url = f'{thredds_server}/thredds/catalog/NHM_CBH_GM/files/catalog.html'
+            # base_url is used to get a list of files for a product
+            # Until xarray supports ncml parsing the list of files will have to be manually built
+            base_url = f'{thredds_server}/thredds/catalog/NHM_CBH_GM/files/catalog.html'
 
-                full_file_list = pd.read_html(base_url, skiprows=1)[0]['Files']
+            full_file_list = pd.read_html(base_url, skiprows=1)[0]['Files']
 
-                # Only include files ending in .nc (sometimes the .ncml files are included and we don't want those)
-                flist = full_file_list[full_file_list.str.match('.*nc$')].tolist()
-                flist.sort()
+            # Only include files ending in .nc (sometimes the .ncml files are included and we don't want those)
+            flist = full_file_list[full_file_list.str.match('.*nc$')].tolist()
+            flist.sort()
 
-                # Create list of file URLs
-                xfiles = [f'{base_opendap}/{xx}' for xx in flist]
+            # Create list of file URLs
+            xfiles = [f'{base_opendap}/{xx}' for xx in flist]
 
-                try:
-                    # self.__dataset = xr.open_mfdataset(self.__src_path,
-                    #                                    chunks={'hruid': 1040}, combine='by_coords',
-                    #                                    decode_cf=True)
-                    # NOTE: With a multi-file dataset the time attributes 'units' and
-                    #       'calendar' are lost.
-                    #       see https://github.com/pydata/xarray/issues/2436
+            try:
+                # NOTE: With a multi-file dataset the time attributes 'units' and
+                #       'calendar' are lost.
+                #       see https://github.com/pydata/xarray/issues/2436
 
-                    # Open the remote multi-file dataset
-                    self.__dataset = xr.open_mfdataset(xfiles, chunks={'hruid': 1040}, combine='by_coords',
-                                                       decode_cf=True, engine='netcdf4')
-                except ValueError:
-                    # self.__dataset = xr.open_mfdataset(self.__src_path, chunks={'hru': 1040}, combine='by_coords')
-                    self.__dataset = xr.open_mfdataset(xfiles, chunks={'hru': 1040}, combine='by_coords',
-                                                       decode_cf=True, engine='netcdf4')
-            else:
-                self.__dataset = xr.open_mfdataset(self.__src_path,
-                                                   chunks={}, combine='by_coords',
-                                                   decode_cf=True, engine='netcdf4')
-                # try:
-                #     self.__dataset = xr.open_mfdataset(self.__src_path,
-                #                                        chunks={'hruid': 1040}, combine='by_coords',
-                #                                        decode_cf=True, engine='netcdf4')
-                #     # NOTE: With a multi-file dataset the time attributes 'units' and
-                #     #       'calendar' are lost.
-                #     #       see https://github.com/pydata/xarray/issues/2436
-                # except ValueError:
-                #     self.__dataset = xr.open_mfdataset(self.__src_path, chunks={'hru': 1040}, combine='by_coords',
-                #                                        decode_cf=True, engine='netcdf4')
+                # Open the remote multi-file dataset
+                ds = xr.open_mfdataset(xfiles, chunks={'hruid': 1040}, combine='by_coords',
+                                       decode_cf=True, engine='netcdf4')
+            except ValueError:
+                # self.__dataset = xr.open_mfdataset(self.__src_path, chunks={'hru': 1040}, combine='by_coords')
+                ds = xr.open_mfdataset(xfiles, chunks={'hru': 1040}, combine='by_coords',
+                                       decode_cf=True, engine='netcdf4')
         else:
-            print('ERROR: write the code for all HRUs')
-            exit()
+            ds = xr.open_mfdataset(self.__src_path, chunks={}, combine='by_coords',
+                                   decode_cf=True, engine='netcdf4')
+
+        if self.__stdate is None and self.__endate is None:
+            # If a date range is not specified then use the daterange from the dataset
+            self.__stdate = pd.to_datetime(str(ds.time[0].values))
+            self.__endate = pd.to_datetime(str(ds.time[-1].values))
+
+        return ds
 
     def get_var(self, var: str) -> pd.DataFrame:
         """
@@ -112,126 +101,53 @@ class CbhNetcdf(object):
                 data = self.__dataset[var].sel(time=slice(self.__stdate, self.__endate), nhru=self.__nhm_hrus).to_pandas()
                 # data = self.__dataset[var].loc[self.__stdate:self.__endate, self.__nhm_hrus].to_pandas()
             except IndexError:
-                print(f'ERROR: Indices (time, hruid) were used to subset {var} which expects' +
+                print(f'ERROR: Indices (time, nhru) were used to subset {var} which expects' +
                       f'indices ({" ".join(map(str, self.__dataset[var].coords))})')
                 raise
+            except KeyError:
+                # Happens when older hruid dimension is used instead of nhru
+                data = self.__dataset[var].sel(time=slice(self.__stdate, self.__endate), hruid=self.__nhm_hrus).to_pandas()
         else:
             print('DEBUG: no dates supplied')
             data = self.__dataset[var].loc[:, self.__nhm_hrus].to_pandas()
 
         return data
 
-    def write_ascii(self, pathname: Optional[str] = None,
-                    fileprefix: Optional[str] = None,
-                    variables: Optional[List[str]] = None):
-        """Write CBH data for variables to ASCII formatted file(s).
 
-        By default CBH filenames are saved in the current working directory and
-        are named for the selected variable with an extension of .cbh
+    def write_ascii(self, filename: str, variable: str):
+        """Write CBH data for variable to ASCII formatted file.
 
-        :param pathname: Path to write files
-        :param fileprefix: prefix to add to CBH output filename
-        :param variables: CBH variable(s) to write to file(s). If None then all variables in netCDF file are output.
+        :param filename: Climate-by-HRU Filename
+        :param variable: CBH variable to write
         """
         # For out_order the first six columns contain the time information and
         # are always output for the cbh files
-        out_order = [kk for kk in self.__nhm_hrus]
+        out_order: List[Union[int, str]] = [kk for kk in self.__nhm_hrus]
         for cc in ['second', 'minute', 'hour', 'day', 'month', 'year']:
             out_order.insert(0, cc)
 
-        var_list = []
-        if variables is None:
-            var_list = self.__dataset.data_vars
-        elif isinstance(variables, list):
-            var_list = variables
+        if variable in self.__dataset.data_vars:
+            data = self.get_var(var=variable)
 
-        for cvar in var_list:
-            if cvar in self.__dataset.data_vars:
-                data = self.get_var(var=cvar)
+            # Add time information as columns
+            data['year'] = data.index.year
+            data['month'] = data.index.month
+            data['day'] = data.index.day
+            data['hour'] = 0
+            data['minute'] = 0
+            data['second'] = 0
 
-                # Add time information as columns
-                data['year'] = data.index.year
-                data['month'] = data.index.month
-                data['day'] = data.index.day
-                data['hour'] = 0
-                data['minute'] = 0
-                data['second'] = 0
+            out_cbh = open(filename, 'w')
+            out_cbh.write('Written by Bandit\n')
+            out_cbh.write(f'{variable} {len(self.__nhm_hrus)}\n')
+            out_cbh.write('########################################\n')
+            # data.to_csv(out_cbh, columns=out_order, na_rep='-999', float_format='%0.3f',
+            data.to_csv(out_cbh, columns=out_order, na_rep='-999', float_format='%0.2f',
+                        sep=' ', index=False, header=False, encoding=None, chunksize=50)
+            out_cbh.close()
+        else:
+            print(f'WARNING: {variable} does not exist in source CBH files..skipping')
 
-                # Output ASCII CBH files
-                if fileprefix is None:
-                    outfile = f'{cvar}.cbh'
-                else:
-                    outfile = f'{fileprefix}_{cvar}.cbh'
-
-                if pathname is not None:
-                    outfile = f'{pathname}/{outfile}'
-
-                out_cbh = open(outfile, 'w')
-                out_cbh.write('Written by Bandit\n')
-                out_cbh.write(f'{cvar} {len(self.__nhm_hrus)}\n')
-                out_cbh.write('########################################\n')
-                # data.to_csv(out_cbh, columns=out_order, na_rep='-999', float_format='%0.3f',
-                data.to_csv(out_cbh, columns=out_order, na_rep='-999', float_format='%0.2f',
-                            sep=' ', index=False, header=False, encoding=None, chunksize=50)
-                out_cbh.close()
-            else:
-                print(f'WARNING: {cvar} does not exist in source CBH files..skipping')
-
-    def write_netcdf_old(self, filename: str = None,
-                         variables: Optional[List[str]] = None):
-        """Write CBH to netcdf format file
-        """
-
-        # NetCDF-related variables
-        var_desc = {'tmax': 'Maximum Temperature', 'tmin': 'Minimum temperature',
-                    'prcp': 'Precipitation', 'rhavg': 'Mean relative humidity'}
-        var_units = {'tmax': 'C', 'tmin': 'C', 'prcp': 'inches', 'rhavg': 'percent'}
-
-        # Create a netCDF file for the CBH data
-        nco = nc.Dataset(filename, 'w', clobber=True)
-        # nco.createDimension('hru', len(self.__dataset['hru'].loc[self.__nhm_hrus]))
-        nco.createDimension('hru', len(self.__nhm_hrus))
-        nco.createDimension('time', None)
-
-        timeo = nco.createVariable('time', 'f4', 'time')
-        timeo.calendar = 'standard'
-        # timeo.bounds = 'time_bnds'
-
-        # FIXME: Days since needs to be set to the starting date of the model pull
-        timeo.units = 'days since 1980-01-01 00:00:00'
-
-        hruo = nco.createVariable('hru', 'i4', 'hru')
-        hruo.long_name = 'Hydrologic Response Unit ID (HRU)'
-
-        var_list = []
-        if variables is None:
-            var_list = self.__dataset.data_vars
-        elif isinstance(list, variables):
-            var_list = variables
-
-        for cvar in var_list:
-            varo = nco.createVariable(cvar, 'f4', ('time', 'hru'), fill_value=nc.default_fillvals['f4'], zlib=True)
-            varo.long_name = var_desc[cvar]
-            varo.units = var_units[cvar]
-
-        nco.setncattr('Description', 'Climate by HRU')
-        # nco.setncattr('Bandit_version', __version__)
-        # nco.setncattr('NHM_version', nhmparamdb_revision)
-
-        # Write the HRU ids
-        hruo[:] = self.__dataset['hru'].loc[self.__nhm_hrus].values
-        hruo[:] = self.__nhm_hrus
-        timeo[:] = nc.date2num(pd.to_datetime(self.__dataset['time'].loc[self.__stdate:self.__endate].values).tolist(),
-                               units='days since 1980-01-01 00:00:00',
-                               calendar='standard')
-
-        for cvar in var_list:
-            data = self.get_var(var=cvar)
-
-            # Write the CBH values
-            nco.variables[cvar][:, :] = data.values
-
-        nco.close()
 
     def write_netcdf(self, filename: str = None,
                      variables: Optional[List[str]] = None):
@@ -247,7 +163,9 @@ class CbhNetcdf(object):
         nco.createDimension('hruid', len(self.__nhm_hrus))
         nco.createDimension('time', None)
 
-        reference_time = self.__stdate.strftime('%Y-%m-%d %H:%M:%S')
+        if self.__stdate is not None:
+            reference_time = self.__stdate.strftime('%Y-%m-%d %H:%M:%S')
+
         cal_type = 'standard'
 
         # Create the variables
@@ -261,9 +179,9 @@ class CbhNetcdf(object):
         hruo.long_name = 'Hydrologic Response Unit ID (HRU)'
         hruo.cf_role = 'timeseries_id'
 
-        var_list = []
+        var_list: List[str] = []
         if variables is None:
-            var_list = self.__dataset.data_vars
+            var_list = list(self.__dataset.data_vars)
         elif isinstance(variables, list):
             var_list = variables
 
@@ -294,7 +212,10 @@ class CbhNetcdf(object):
         hruo[:] = self.__nhm_hrus
 
         # Write time information
-        timeo[:] = nc.date2num(pd.to_datetime(self.__dataset['time'].loc[self.__stdate:self.__endate].values).tolist(),
+        # timeo[:] = nc.date2num(pd.to_datetime(self.__dataset['time'].loc[self.__stdate:self.__endate].values).tolist(),
+        #                units=f'days since {reference_time}',
+        #                calendar=cal_type)
+        timeo[:] = nc.date2num(pd.to_datetime(self.__dataset['time'].sel(time=slice(self.__stdate, self.__endate)).values).tolist(),
                                units=f'days since {reference_time}',
                                calendar=cal_type)
 
