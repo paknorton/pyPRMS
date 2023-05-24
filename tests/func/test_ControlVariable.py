98277fe3085900370fb1fc05beeaef8706163a18
Parker Norton <pnorton@usgs.gov>
2023-05-24 11:55:34 -0600

    Update tests for metadata changes

--- tests/func/test_ControlVariable.py
+++ tests/func/test_ControlVariable.py
@@ -1,7 +1,8 @@
 import pytest
-import numpy as np
+# import numpy as np
 from pyPRMS import ControlVariable
-from pyPRMS.constants import NEW_PTYPE_TO_DTYPE   # PTYPE_TO_DTYPE
+from pyPRMS import MetaData
+# from pyPRMS.constants import NEW_PTYPE_TO_DTYPE   # PTYPE_TO_DTYPE
 
 metadata = {'int_val': {'datatype': 'int32', 'context': 'scalar', 'default': 1, 'force_default': False},
             'float32_val': {'datatype': 'float32', 'context': 'scalar', 'default': 2.3, 'force_default': False},
@@ -15,11 +16,14 @@ class TestControlVariable:
     # Add control variable
     # Add value(s) to control variable
 
-    @pytest.mark.parametrize('name', ['int_val',
-                                      'float32_val',
-                                      'str_val'])
+    # @pytest.mark.parametrize('name', ['int_val',
+    #                                   'float32_val',
+    #                                   'str_val'])
+    @pytest.mark.parametrize('name', ['prms_warmup'])
     def test_create_control_variable(self, name):
-        avar = ControlVariable(name, meta=metadata[name])
+        prms_meta = MetaData().metadata['control']
+
+        avar = ControlVariable(name, meta=prms_meta[name])
 
         # A control variable with no value assigned should return the default value
         assert avar.name == name and \
@@ -46,21 +50,25 @@ class TestControlVariable:
     #     with pytest.raises(ValueError):
     #         avar = ControlVariable(name, datatype=dtype, default=def_val)
 
-    @pytest.mark.parametrize('name, val', [('int_val', 7),
-                                           ('float32_val', 9.2),
-                                           ('str_val', 'goodbye')])
+    @pytest.mark.parametrize('name, val', [('prms_warmup', 8),
+                                           ('et_module', 'potet_hamon'),
+                                           ('initial_deltat', 20.2)])
     def test_set_value(self, name, val):
-        avar = ControlVariable(name, meta=metadata[name])
+        prms_meta = MetaData().metadata['control']
+
+        avar = ControlVariable(name, meta=prms_meta[name])
         avar.values = val
 
         assert avar.meta['default'] != avar.values
 
-    @pytest.mark.parametrize('name, val', [('int_val', 7),
-                                           ('float32_val', 9.2),
-                                           ('str_val', 'goodbye')])
+    @pytest.mark.parametrize('name, val', [('prms_warmup', 8),
+                                           ('et_module', 'potet_hamon'),
+                                           ('initial_deltat', 20.2)])
     def test_force_default(self, name, val):
         """Always return the default value when force_default is set"""
-        avar = ControlVariable(name, meta=metadata[name])
+        prms_meta = MetaData().metadata['control']
+
+        avar = ControlVariable(name, meta=prms_meta[name])
         avar.values = val
 
         assert avar.meta['default'] != avar.values
