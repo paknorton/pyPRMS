36004898bb56022e5036f128148954b12849a74a
Parker Norton <pnorton@usgs.gov>
2023-07-07 16:09:04 -0600

    Changes to support metadata
    - add strict argument to object
    - cleanup __str__
    - values setter; add exception when incoming datatype does not match expected datatype
    - value_meanings()
      - can now detect an invalid value when `valid_values` metadata is populated
      - can test  conditional valid values

--- pyPRMS/control/ControlVariable.py
+++ pyPRMS/control/ControlVariable.py
@@ -2,10 +2,11 @@
 
 import datetime
 import numpy as np
+import re
 from typing import Callable, Dict, List, Optional, Sequence, Union
 
 from ..constants import DATA_TYPES, NEW_PTYPE_TO_DTYPE
-from pyPRMS.prms_helpers import set_date
+from pyPRMS.prms_helpers import cond_check, set_date
 from ..Exceptions_custom import ControlError
 
 class ControlVariable(object):
@@ -17,7 +18,9 @@ class ControlVariable(object):
     # Create date: 2019-04-18
 
     def __init__(self, name: str,
-                 meta: Optional[Dict] = None):
+                 value = None,
+                 meta: Optional[Dict] = None,
+                 strict: Optional[bool] = True):
         """Initialize a control variable object.
 
         :param name: Name of control variable
@@ -26,26 +29,50 @@ class ControlVariable(object):
 
         self.__name = name
         self.__values: Union[np.ndarray, None] = None
-        self.meta = meta
+
+        if meta is None:
+            if strict:
+                raise ValueError(f'Strict is true but no metadata was supplied')
+            else:
+                self.meta = {}
+        else:
+            if strict:
+                if name in meta:
+                    self.meta = meta[name]
+                else:
+                    raise ValueError(f'`{self.name}` does not exist in metadata')
+            else:
+                # The meta must be supplied as an adhoc dictionary
+                self.meta = meta
+
+        self.values = value
 
     def __str__(self) -> str:
-        outstr = f'name: {self.name}\n'
-        if self.meta is not None:
-            outstr += f'datatype: {self.meta.get("datatype")}\n'
-            # outstr += f'datatype: {self.datatype} ({DATA_TYPES[self.datatype]})\n'
+        outstr = f'----- ControlVariable -----\n'
+        outstr += f'name: {self.name}\n'
 
-            outstr += f'description: {self.meta["description"]}\n'
-            outstr += f'default: {self.meta["default"]}\n'
-
-            if 'valid_value_type' in self.meta:
-                outstr += f'valid values represent: {self.meta["valid_value_type"]}\n'
-
-            if 'valid_values' in self.meta:
-                outstr += f'valid_values: \n'
-                for kk, vv in self.meta['valid_values'].items():
-                    outstr += f'   {kk}: {vv}\n'
+        for kk, vv in self.meta.items():
+            outstr += f'{kk}: {vv}\n'
 
         return outstr
+
+        # outstr = f'name: {self.name}\n'
+        # if self.meta is not None:
+        #     outstr += f'datatype: {self.meta.get("datatype")}\n'
+        #     # outstr += f'datatype: {self.datatype} ({DATA_TYPES[self.datatype]})\n'
+        #
+        #     outstr += f'description: {self.meta["description"]}\n'
+        #     outstr += f'default: {self.meta["default"]}\n'
+        #
+        #     if 'valid_value_type' in self.meta:
+        #         outstr += f'valid values represent: {self.meta["valid_value_type"]}\n'
+        #
+        #     if 'valid_values' in self.meta:
+        #         outstr += f'valid_values: \n'
+        #         for kk, vv in self.meta['valid_values'].items():
+        #             outstr += f'   {kk}: {vv}\n'
+        #
+        # return outstr
 
     # @property
     # def associated_values(self) -> List[str]:
@@ -87,6 +114,7 @@ class ControlVariable(object):
         else:
             # int, float, str scalars
             return 1
+
     @property
     def values(self) -> Union[np.ndarray, int, float, str, None]:
         """Get the values for the control variable.
@@ -97,18 +125,12 @@ class ControlVariable(object):
         :returns: Value(s) of control variable
         """
 
-        if self.meta is not None:
-            force_default = self.meta.get('force_default', False)
+        force_default = self.meta.get('force_default', False)
 
-            if force_default:
-                return self.meta['default']
-
-            if self.__values is not None:
-                return self.__values
-            else:
-                return self.meta.get('default', None)
-
-        return None
+        if force_default or self.__values is None:
+            return self.meta.get('default', None)
+        else:
+            return self.__values
 
     @values.setter
     def values(self, data: Union[Sequence[str], str, int, float, datetime.datetime]):
@@ -117,19 +139,28 @@ class ControlVariable(object):
         :param data: list or string of value(s)
         """
 
-        if self.meta is not None:
+        # if self.meta is not None:
+        if data is None:
+            self.__values = self.meta.get('default', None)
+        else:
             cdtype = NEW_PTYPE_TO_DTYPE[self.meta['datatype']]
 
             if self.meta['context'] == 'scalar':
                 if isinstance(data, list):
+                    # Only first value is used for scalars
                     self.__values = np.array(data, dtype=cdtype)[0]
                 elif isinstance(data, np.ndarray):
-                    if data.dtype == cdtype:
-                        self.__values = data[0]
-                    elif self.meta['datatype'] == 'datetime':
+                    if self.meta['datatype'] == 'datetime':
                         self.__values = cdtype(set_date(data))
+                    elif data.dtype == cdtype:
+                        self.__values = data[0]
                     else:
-                        print(f'WARNING: {self.name} - datatype inconsistency; no values added')
+                        raise TypeError(f'{self.name}: {cdtype} expected but got {data[0].dtype}')
+                        # print(f'WARNING: {self.name} - datatype inconsistency; no values added')
+                elif ((cdtype == np.str_ and not isinstance(data, str | cdtype)) or
+                      (cdtype == np.int32 and not isinstance(data, int | cdtype)) or
+                      (cdtype == np.float32 and not isinstance(data, float | cdtype))):
+                    raise TypeError(f'{self.name}: {cdtype} expected but got {type(data)}')
                 else:
                     self.__values = cdtype(data)
             else:
@@ -137,15 +168,14 @@ class ControlVariable(object):
                     self.__values = np.array(data, dtype=cdtype)
                 elif isinstance(data, np.ndarray):
                     if data.dtype == cdtype:
+                        # TODO: 20230707 PAN - currently never hit with string arrays
                         self.__values = data
                     else:
                         # Attempt to convert to correct datatype
                         self.__values = np.array(data, dtype=cdtype)
                 else:
+                    # TODO: 20230707 PAN - currently never hit
                     self.__values = np.array([data], dtype=cdtype)
-        else:
-            # TODO: 2023-05-24 PAN - probably raise an error if this happens
-            print('ERROR: meta is not defined; values not added.')
 
     @property
     def value_meaning(self) -> Union[str, None]:
@@ -154,15 +184,48 @@ class ControlVariable(object):
         :returns: Control variable value meaning
         """
 
-        if self.meta is not None:
-            # This will fail for keys that are conditionals (e.g. ">0")
-            try:
-                if 'valid_values' in self.meta:
-                    # We want a KeyError here if the key is missing
-                    return self.meta['valid_values'][self.values]
+        # This will fail for keys that are conditionals (e.g. ">0")
+        meaning = self.meta.get('valid_values', None)
 
-                return None
+        if meaning is None:
+            # valid_values key does not exist
+            return meaning
+
+        return self._value_meaning_test(self.values, meaning)
+        # return meaning.get(self.values, meaning.get(str(self.values), None))
+
+    def _value_meaning_test(self, key, src_dict):
+        try:
+            return src_dict[key]
+        except KeyError:
+            # Maybe the key is a string
+            try:
+                return src_dict[str(key)]
             except KeyError:
-                # Try again but return None if the key is still missing
-                return self.meta['valid_values'].get(str(self.values), None)
-        return None
+                # Maybe one of the keys is a conditional?
+                patterns = ['[><]']
+                regex = [re.compile('^' + pat).match for pat in patterns]
+
+                tt = {kk: vv for kk, vv in src_dict.items()
+                      if any (reg(kk) for reg in regex)}
+
+                if len(tt) > 0:
+                    # So there is a conditional
+                    for mm in tt:
+                        # print(mm.split())
+                        if cond_check[mm[0]](key, int(mm[1:])):
+                            return src_dict[mm]
+                            # print(f'{mm}: {src_dict[mm]}')
+        raise ValueError('Invalid control value')
+
+
+        #     try:
+        #         if 'valid_values' in self.meta:
+        #             # We want a KeyError here if the key is missing
+        #             return self.meta['valid_values'][self.values]
+        #
+        #         return None
+        #     except KeyError:
+        #         # Try again but return None if the key is still missing
+        #         return self.meta['valid_values'].get(str(self.values), None)
+        # return None
