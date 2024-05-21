4c0274e4b5cd22f93f36ac9b90218bb2d9fd5fc1
Parker Norton <pnorton@usgs.gov>
2024-05-21 07:53:12 -0600

    Add tests for remove_poi
    - add npoigages dimension to test_add_global_dimensions()
    - add poi_gage_id, poi_gage_segment, and poi_type to test_add_valid_parameter()
    - add poi_gage_id, poi_gage_segment, and poi_type to test_parameter_data()
    - add test_remove_poi_list(), test_remove_poi_str(), test_remove_poi_all()

--- tests/func/test_Parameters.py
+++ tests/func/test_Parameters.py
@@ -24,7 +24,8 @@ class TestParameters:
 
     @pytest.mark.parametrize('name, size', [('nhru', 4),
                                             ('nmonths', 12),
-                                            ('one', 1)])
+                                            ('one', 1),
+                                            ('npoigages', 4)])
     def test_add_global_dimensions(self, pdb_instance, name, size):
         pdb_instance.dimensions.add(name=name, size=size)
 
@@ -32,7 +33,10 @@ class TestParameters:
 
     @pytest.mark.parametrize('name', [('cov_type'),
                                       ('tmax_adj'),
-                                      ('basin_solsta')])
+                                      ('basin_solsta'),
+                                      ('poi_gage_id'),
+                                      ('poi_gage_segment'),
+                                      ('poi_type')])
     def test_add_valid_parameter(self, pdb_instance, name):
         pdb_instance.add(name=name)
         assert pdb_instance.exists(name=name)
@@ -46,7 +50,10 @@ class TestParameters:
             pdb_instance.add(name=name)
     @pytest.mark.parametrize('name, data', [('cov_type', np.array([1, 0, 1, 2], dtype=np.int32)),
                                             ('tmax_adj', np.zeros(48, dtype=np.float32).reshape((-1, 12), order='F')),
-                                            ('basin_solsta', np.int32(8))])
+                                            ('basin_solsta', np.int32(8)),
+                                            ('poi_gage_id', np.array(['01234567', '12345678', '23456789', '34567890'], dtype=np.str_)),
+                                            ('poi_gage_segment', np.array([12, 4, 45, 26], dtype=np.int32)),
+                                            ('poi_type', np.array([1, 0, 1, 0], dtype=np.int32))])
     def test_parameter_data(self, pdb_instance, name, data):
         pdb_instance[name].data = data
 
@@ -58,3 +65,25 @@ class TestParameters:
     def test_get_missing_parameter(self, pdb_instance):
         with pytest.raises(ParameterError):
             aa = pdb_instance.get('nothin')
+
+    def test_remove_poi_list(self, pdb_instance):
+        pdb_instance.remove_poi(['12345678'])
+        assert (pdb_instance['poi_gage_id'].data == np.array(['01234567', '23456789', '34567890'], dtype=np.str_)).all()
+        assert (pdb_instance['poi_gage_segment'].data == np.array([12, 45, 26], dtype=np.int32)).all()
+        assert (pdb_instance['poi_type'].data == np.array([1, 1, 0], dtype=np.int32)).all()
+        assert (pdb_instance.dimensions.get('npoigages').size == pdb_instance['poi_gage_id'].data.size)
+
+    def test_remove_poi_str(self, pdb_instance):
+        pdb_instance.remove_poi('34567890')
+        assert (pdb_instance['poi_gage_id'].data == np.array(['01234567', '23456789'], dtype=np.str_)).all()
+        assert (pdb_instance['poi_gage_segment'].data == np.array([12, 45], dtype=np.int32)).all()
+        assert (pdb_instance['poi_type'].data == np.array([1, 1], dtype=np.int32)).all()
+        assert (pdb_instance.dimensions.get('npoigages').size == pdb_instance['poi_gage_id'].data.size)
+
+    def test_remove_poi_all(self, pdb_instance):
+        """When all POIs are removed, the parameters should be removed and the npoigages dimension should be zero."""
+        pdb_instance.remove_poi(['01234567', '23456789'])
+        assert not pdb_instance.exists('poi_gage_id')
+        assert not pdb_instance.exists('poi_gage_segment')
+        assert not pdb_instance.exists('poi_type')
+        assert (pdb_instance.dimensions.get('npoigages').size == 0)
