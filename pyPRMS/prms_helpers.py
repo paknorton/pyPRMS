5e4f54b431d093d77d34b7332bc3f6af1b9c5733
Parker Norton <pnorton@usgs.gov>
2024-08-02 12:00:50 -0600

    Add stream network subsetting
    - get_streamnet_subset()

--- pyPRMS/prms_helpers.py
+++ pyPRMS/prms_helpers.py
@@ -1,22 +1,24 @@
 
 from collections import namedtuple
 
-from typing import List, NamedTuple, Optional, Union, Sequence
+from typing import List, NamedTuple, Optional, Set, Union, Sequence
 
-import calendar
+# import calendar
 import datetime
 import decimal
 import operator
 import pandas as pd   # type: ignore
+import networkx as nx   # type: ignore
 import numpy as np
 import re
 import xml.etree.ElementTree as xmlET
 
-from .constants import Version
+from .constants import Version   # type: ignore
 
 cond_check = {'=': operator.eq,
               '>': operator.gt,
               '<': operator.lt}
+
 
 def flex_type(val):
     if isinstance(val, str):
@@ -27,6 +29,7 @@ def flex_type(val):
         except decimal.InvalidOperation:
             print(f'Caused by: {val}')
             raise
+
 
 def float_to_str(f: float) -> str:
     """Convert the given float to a string, without resorting to scientific notation.
@@ -47,15 +50,99 @@ def float_to_str(f: float) -> str:
     d1 = ctx.create_decimal(repr(f))
     return format(d1, 'f')
 
+
 def get_file_iter(filename):
-    '''Reads a file and returns an iterator to the data
-    '''
+    """Reads a file and returns an iterator to the data
+    """
 
     infile = open(filename, 'r')
     rawdata = infile.read().splitlines()
     infile.close()
 
     return iter(rawdata)
+
+
+def get_streamnet_subset(dag_ds: nx.classes.digraph.DiGraph,
+                         uscutoff_seg: List[int],
+                         dsmost_seg: List[int]) -> nx.classes.digraph.DiGraph:
+    """Extract subset of a stream network
+
+    :param dag_ds: Directed, acyclic graph of downstream stream network
+    :param uscutoff_seg: List of upstream cutoff segments
+    :param dsmost_seg: List of outlet segments to start extraction from
+
+    :returns: Stream network of extracted segments
+    """
+    # Create the upstream graph
+    # TODO: 2021-12-01 PAN - the reverse function is pretty inefficient for multi-location
+    #       jobs.
+    dag_us = dag_ds.reverse()
+    # logger.debug('Number of NHM upstream nodes: {}'.format(dag_us.number_of_nodes()))
+    # logger.debug('Number of NHM upstream edges: {}'.format(dag_us.number_of_edges()))
+
+    # Trim the u/s graph to remove segments above the u/s cutoff segments
+    try:
+        for xx in uscutoff_seg:
+            try:
+                dag_us.remove_nodes_from(nx.dfs_predecessors(dag_us, xx))
+
+                # Also remove the cutoff segment itself
+                dag_us.remove_node(xx)
+            except KeyError:
+                print(f'WARNING: nhm_segment {xx} does not exist in stream network')
+    except TypeError:
+        print('\nSelected cutoffs should at least be an empty list instead of NoneType.')
+        # logger.error('\nSelected cutoffs should at least be an empty list instead of NoneType.')
+        # exit(200)
+
+    # logger.debug('Number of NHM upstream nodes (trimmed): {}'.format(dag_us.number_of_nodes()))
+    # logger.debug('Number of NHM upstream edges (trimmed): {}'.format(dag_us.number_of_edges()))
+
+    # =======================================
+    # Given a d/s segment (dsmost_seg) create a subset of u/s segments
+
+    # Get all unique segments u/s of the starting segment
+    uniq_seg_us: Set[int] = set()
+    if dsmost_seg:
+        for xx in dsmost_seg:
+            try:
+                pred = nx.dfs_predecessors(dag_us, xx)
+                uniq_seg_us = uniq_seg_us.union(set(pred.keys()).union(set(pred.values())))
+            except KeyError:
+                # logger.error('KeyError: Segment {} does not exist in stream network'.format(xx))
+                print(f'KeyError: Segment {xx} does not exist in stream network')
+
+        # Get a subgraph in the dag_ds graph and return the edges
+        dag_ds_subset = dag_ds.subgraph(uniq_seg_us).copy()
+
+        # 2018-02-13 PAN: It is possible to have outlets specified which are not truly
+        #                 outlets in the most conservative sense (e.g. a point where
+        #                 the stream network exits the study area). This occurs when
+        #                 doing headwater extractions where all segments for a headwater
+        #                 are specified in the configuration file. Instead of creating
+        #                 output edges for all specified 'outlets' the set difference
+        #                 between the specified outlets and nodes in the graph subset
+        #                 which have no edges is performed first to reduce the number of
+        #                 outlets to the 'true' outlets of the system.
+        node_outlets = [ee[0] for ee in dag_ds_subset.edges()]
+        true_outlets = set(dsmost_seg).difference(set(node_outlets))
+        # logger.debug('node_outlets: {}'.format(','.join(map(str, node_outlets))))
+        # logger.debug('true_outlets: {}'.format(','.join(map(str, true_outlets))))
+
+        # Add the downstream segments that exit the subgraph
+        for xx in true_outlets:
+            nhm_outlet = list(dag_ds.neighbors(xx))[0]
+            dag_ds_subset.add_node(nhm_outlet, style='filled', fontcolor='white', fillcolor='grey')
+            dag_ds_subset.add_edge(xx, nhm_outlet)
+            dag_ds_subset.nodes[xx]['style'] = 'filled'
+            dag_ds_subset.nodes[xx]['fontcolor'] = 'white'
+            dag_ds_subset.nodes[xx]['fillcolor'] = 'blue'
+    else:
+        # No outlets specified so pull the CONUS
+        dag_ds_subset = dag_ds
+
+    return dag_ds_subset
+
 
 def read_xml(filename: str) -> xmlET.Element:
     """Returns the root of the xml tree for a given file.
@@ -68,6 +155,7 @@ def read_xml(filename: str) -> xmlET.Element:
     # Open and parse an xml file and return the root of the tree
     xml_tree = xmlET.parse(filename)
     return xml_tree.getroot()
+
 
 def set_date(adate: Union[datetime.datetime, datetime.date, str]) -> datetime.datetime:
     """Return datetime object given a datetime or string of format YYYY-MM-DD
@@ -84,6 +172,7 @@ def set_date(adate: Union[datetime.datetime, datetime.date, str]) -> datetime.da
         return datetime.datetime(*adate)
     else:
         return datetime.datetime(*[int(x) for x in re.split('[- :]', adate)])  # type: ignore
+
 
 def version_info(version_str: Optional[str] = None, delim: Optional[str] = '.') -> Version:
     """Given a version string (MM.mm.rr) returns a named tuple of version values
