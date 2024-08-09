10c4327ee48b09d10fe7ca97f5ee4d327be139ec
Parker Norton <pnorton@usgs.gov>
2024-08-09 09:09:33 -0600

    Add exception FixedDimensionError

--- pyPRMS/Exceptions_custom.py
+++ pyPRMS/Exceptions_custom.py
@@ -19,6 +19,12 @@ class ParameterNotValidError(Exception):
         Exception.__init__(self, err_args)
         self.errArgs = err_args
 
+class FixedDimensionError(Exception):
+    """Raised when attempting to modify a fixed dimension"""
+    def __init__(self, err_args):
+        Exception.__init__(self, err_args)
+        self.errArgs = err_args
+
 class ControlError(Exception):
     def __init__(self, err_args):
         Exception.__init__(self, err_args)
