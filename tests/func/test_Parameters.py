46499d5765cb1b70d7b4597efbb626bf267bf394
Parker Norton <pnorton@usgs.gov>
2024-08-12 09:05:33 -0600

    Multiple updates
    - new tests added
      - test_add_parameter_missing_global()
      - test_xml_global_dimensions()
      - test_xml_global_parameters()
      - test_parameters_str()
      - test_add_adhoc_parameter_metadata()

--- tests/func/test_Parameters.py
+++ tests/func/test_Parameters.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 import pandas as pd
+import xml.etree.ElementTree as xmlET
 from pyPRMS import Parameters
 from pyPRMS.Exceptions_custom import ParameterError, ParameterExistsError
 from pyPRMS import MetaData
@@ -23,6 +24,10 @@ class TestParameters:
         with pytest.raises(AssertionError):
             pdb_instance._read()
 
+    def test_add_parameter_missing_global(self, pdb_instance):
+        with pytest.raises(KeyError):
+            pdb_instance.add('basin_solsta')
+
     @pytest.mark.parametrize('name, size', [('nhru', 4),
                                             ('nmonths', 12),
                                             ('one', 1),
@@ -32,6 +37,19 @@ class TestParameters:
         pdb_instance.dimensions.add(name=name, size=size)
 
         assert pdb_instance.dimensions.get(name).size == size
+
+    def test_xml_global_dimensions(self, pdb_instance):
+        xml = pdb_instance.xml_global_dimensions
+        expected_xml = b'<dimensions><dimension name="nhru"><description>Number of HRUs</description><size>4</size><default>1</default></dimension><dimension name="nmonths"><description>Number of months in a year</description><size>12</size><default>12</default></dimension><dimension name="one"><description>Dimension of scalar parameters and variables</description><size>1</size><default>1</default></dimension><dimension name="npoigages"><description>Number of POI gages</description><size>4</size><default>0</default></dimension><dimension name="nobs"><description>Number of streamflow-measurement stations</description><size>4</size><default>0</default></dimension></dimensions>'
+        assert xmlET.tostring(xml) == expected_xml
+
+    def test_xml_global_parameters(self, pdb_instance):
+        pdb_instance.add(name='tmax_cbh_adj')
+
+        xml = pdb_instance.xml_global_parameters
+        expected_xml = b'<parameters><parameter name="tmax_cbh_adj"><type>F</type><description>Monthly maximum temperature adjustment factor for each HRU</description><help>Monthly (January to December) adjustment factor to maximum air temperature for each HRU, estimated on the basis of slope and aspect</help><units>temp_units</units><default>0.0</default><minimum>-10.0</minimum><maximum>10.0</maximum><dimensions><dimension name="nhru"><position>1</position><size>4</size></dimension><dimension name="nmonths"><position>2</position><size>12</size></dimension></dimensions><modules><module>temperature_hru</module></modules></parameter></parameters>'
+
+        assert xmlET.tostring(xml) == expected_xml
 
     @pytest.mark.parametrize('name', [('cov_type'),
                                       ('tmin_cbh_adj'),
@@ -44,6 +62,11 @@ class TestParameters:
         pdb_instance.add(name=name)
         assert pdb_instance.exists(name=name)
 
+    def test_parameters_str(self, pdb_instance):
+        expected = '----- Dimensions -----\nnhru: size=4\nnmonths: size=12\none: size=1\nnpoigages: size=4\nnobs: size=4\n----- Parameters -----\ntmax_cbh_adj [nhru, nmonths]\ncov_type [nhru]\ntmin_cbh_adj [nhru, nmonths]\ntmax_adj [nhru, nmonths]\nbasin_solsta [one]\npoi_gage_id [npoigages]\npoi_gage_segment [npoigages]\npoi_type [npoigages]\n'
+
+        assert pdb_instance.__str__() == expected
+
     @pytest.mark.parametrize('name', [('cov_type'),
                                       ('tmax_adj'),
                                       ('basin_solsta')])
@@ -51,6 +74,7 @@ class TestParameters:
         # Trying to add a parameter that already exists should raise an error
         with pytest.raises(ParameterExistsError):
             pdb_instance.add(name=name)
+
     @pytest.mark.parametrize('name, data', [('cov_type', np.array([1, 0, 1, 2], dtype=np.int32)),
                                             ('tmin_cbh_adj', np.array([[1.0, 2.1, 3.2, 4.3, 5.4, 6.5, 7.6, 8.7, 9.8, 10.9, 11.1, 12.11],
                                                                        [13.12, 14.13, 15.14, 16.15, 17.16, 18.17, 19.18, 20.19, 21.20, 22.21, 23.22, 24.23],
@@ -129,3 +153,13 @@ class TestParameters:
         assert not pdb_instance.exists('poi_gage_segment')
         assert not pdb_instance.exists('poi_type')
         assert (pdb_instance.dimensions.get('npoigages').size == 0)
+
+    def test_add_adhoc_parameter_metadata(self, pdb_instance):
+        entry_items = dict(datatype='float32', description='something new today', help='get your own help',
+                           units='dontmatter', default=0.0, minimum=0.0, maximum=100.0, dimensions=['nhru'])
+        expected = "----- Parameter -----\nname: foo\ndatatype: float32\ndescription: something new today\nhelp: get your own help\nunits: dontmatter\ndefault: 0.0\nminimum: 0.0\nmaximum: 100.0\ndimensions: ['nhru']\n"
+
+        pdb_instance.add_metadata('foo', entry_items)
+        pdb_instance.add('foo')
+
+        assert pdb_instance.get('foo').__str__() == expected
