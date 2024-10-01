9e32e92e50eb046dec69b0a3fbe18b91b856693f
Parker Norton <pnorton@usgs.gov>
2024-10-01 14:17:05 -0600

    change Streamflow to DataFile

--- pyPRMS/__init__.py
+++ pyPRMS/__init__.py
@@ -1,3 +1,4 @@
+
 from . import constants
 from . import Exceptions_custom
 from . import plot_helpers
@@ -16,33 +17,32 @@ from .parameters.ParamDb import ParamDb
 from .parameters.ParameterNetCDF import ParameterNetCDF
 from .cbh.CbhAscii import CbhAscii
 from .cbh.CbhNetcdf import CbhNetcdf
+from .DataFile import DataFile
 
 
 from .version import __author__, __author_email__, __version__
 
-__all__ = [
-    "constants",
-    "Exceptions_custom",
-    "plot_helpers",
-    "prms_helpers",
-    "control",
-    "dimensions",
-    "parameters",
-    "cbh",
-    "CbhAscii",
-    "CbhNetcdf",
-    "Control",
-    "ControlFile",
-    "ControlVariable",
-    "Dimension",
-    "Dimensions",
-    "MetaData",
-    "OutputVariables",
-    "ParamDimensions",
-    "Parameter",
-    "Parameters",
-    "ParameterFile",
-    "ParamDb",
-    "ParameterNetCDF",
-    "Streamflow",
-]
+__all__ = ['constants',
+           'Exceptions_custom',
+           'plot_helpers',
+           'prms_helpers',
+           'control',
+           'dimensions',
+           'parameters',
+           'cbh',
+           'CbhAscii',
+           'CbhNetcdf',
+           'Control',
+           'ControlFile',
+           'ControlVariable',
+           'Dimension',
+           'Dimensions',
+           'MetaData',
+           'OutputVariables',
+           'ParamDimensions',
+           'Parameter',
+           'Parameters',
+           'ParameterFile',
+           'ParamDb',
+           'ParameterNetCDF',
+           'DataFile']
