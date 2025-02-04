84e984811cb812786c2602aafc01f38177cac8a5
Parker Norton <pnorton@usgs.gov>
2025-02-04 13:17:40 -0700

    Change version arguments to strings

--- tests/func/test_MetaData.py
+++ tests/func/test_MetaData.py
@@ -6,14 +6,14 @@ from pyPRMS import MetaData
 class TestMetaData:
 
     def test_metadata_version(self):
-        """Test reading metadata when version > 5"""
-        prms_meta = MetaData(version=6, verbose=True).metadata
+        """Test reading metadata when version > 6"""
+        prms_meta = MetaData(version='6', verbose=True).metadata
         assert prms_meta['control'].get('stat_var_file', None) is None
 
     def test_metadata_version5(self):
-        prms_meta = MetaData(version=5, verbose=True).metadata
+        prms_meta = MetaData(version='5.2.1.1', verbose=True).metadata
         assert prms_meta['parameters'].get('outVarON_OFF', None) is None
 
     def test_metadata_version4(self):
-        prms_meta = MetaData(version=4, verbose=True).metadata
+        prms_meta = MetaData(version='4', verbose=True).metadata
         assert prms_meta['parameters'].get('soilzone_aet_flag', None) is None
