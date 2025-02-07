5e2832737f2997e085efb3c7ca40b21bd2ec9d20
Parker Norton <pnorton@usgs.gov>
2025-02-07 10:29:23 -0700

    Updates to support PRMS-classic version 6.0.0
    - testing for the earlier prms6 development code is done by setting the version to 60.0

--- tests/func/test_MetaData.py
+++ tests/func/test_MetaData.py
@@ -5,10 +5,19 @@ from pyPRMS import MetaData
 
 class TestMetaData:
 
-    def test_metadata_version(self):
-        """Test reading metadata when version > 6"""
-        prms_meta = MetaData(version='6', verbose=True).metadata
+    def test_metadata_version_ref6(self):
+        """Test reading metadata when version == 60.0
+
+        PRMS-classic released a version 6.0.0 in 2025-02 which conflicts
+        with the earlier prms6 refactored development code. Metadata version information
+        for the earlier development code was changed from 6.0 to 60.0 to avoid conflicts.
+        """
+        prms_meta = MetaData(version='60.0', verbose=True).metadata
         assert prms_meta['control'].get('stat_var_file', None) is None
+
+    def test_metadata_version6(self):
+        prms_meta = MetaData(version='6.0.0', verbose=True).metadata
+        assert prms_meta['parameters'].get('outVarON_OFF', None) is None
 
     def test_metadata_version5(self):
         prms_meta = MetaData(version='5.2.1.1', verbose=True).metadata
