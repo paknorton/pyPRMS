de0c662a202802be432922e13596a1b7e2e5069f
Parker Norton <pnorton@usgs.gov>
2025-05-09 09:22:53 -0600

    feat: refactor OutputCSV
    - remove need for specifying csvON_OFF value on init
    - now autodetects file structure

--- pyPRMS/summary/OutputCSV.py
+++ pyPRMS/summary/OutputCSV.py
@@ -1,64 +1,140 @@
-import pandas as pd
+import pandas as pd   # type: ignore
+
+from pathlib import Path
+from typing import Optional, Union
+from rich.console import Console
+from rich import pretty
+
+pretty.install()
+con = Console(force_jupyter=False)
 
 
 class OutputCSV(object):
     """Class for working with PRMS CSV output files.
     """
 
-    def __init__(self, filename: str, on_off: int):
+    def __init__(self, filename: Union[str, Path],
+                 verbose: Optional[bool] = False):
         """Initialize the OutputCSV object.
 
         :param filename: Name of the PRMS CSV output file
-        :param on_off: ON_OFF flag for the PRMS model (1=comma-sep (basin vars + streamflow), 2=space-sep (streamflow only))
+        :param verbose: Output debugging information
         """
+
         self.__filename = filename
-        self.__on_off = on_off
+
+        if isinstance(self.__filename, str):
+            self.__filename = Path(self.__filename)
+
+        self.verbose = verbose
         self.__data = None
 
-    def _read_streamflow_header(self, filename):
+        self.__pois = []
+        self.__poi_segments = {}
+        self.__basin_vars = []
+        self.__col_var = {}
+
+        self._read_csv_header()
+        self._read_csv_ascii()
+
+    @property
+    def basin_vars(self):
+        return self.__basin_vars
+
+    @property
+    def data(self):
+        return self.__data
+
+    @property
+    def pois(self):
+        return self.__pois
+
+    @property
+    def poi_segments(self):
+        return self.__poi_segments
+
+    @property
+    def variables(self):
+        return sorted(list(self.__col_var.values()))
+
+    def _read_csv_header(self):
         """Read the headers from a PRMS CSV model output file"""
 
-        fhdl = open(filename, 'r')
+        fhdl = open(self.__filename, 'r')
 
-        # First and second rows are headers
+        # First row contains field names
+        # Second row is a a mix of field names (for the date) and data types
         hdr1 = fhdl.readline().strip()
-
+        hdr2 = fhdl.readline().strip()
         fhdl.close()
 
-        if self.__on_off == 1:
-            # This is a comma-separated file that includes a selection
-            # of basin variables and simulated streamflow at each POI.
-            pass
-        elif self.__on_off == 2:
-            # This is a space-separated file that includes only the simulated
-            # streamflow at each POI.
-            tmp_flds = hdr1.split(' ')
-            tmp_flds.remove('Date')
+        # Determine the value separator
+        # Check for comma first; some files have commas and spaces
+        self.sep = ' '
 
-            flds = {nn+3: hh for nn, hh in enumerate(tmp_flds)}
+        match ',' in hdr1:
+            case True:
+                self.sep = ','
 
-            # poi_flds maps column index to POI and is used to rename the dataframe columns from indices to station IDs
-            poi_flds = dict()
+        if self.verbose:
+            con.print(f'value separator: {self.sep}')
 
-            # poi_seg_flds maps POI to the related segment ID
-            poi_seg_flds = dict()
+        # Determine the format of the time field(s)
+        var_offset = 0
+        if 'year month day' in hdr2:
+            var_offset = 3
+            self.time_col_names = {0: 'year', 1: 'month', 2: 'day'}
+        elif 'year-month-day' in hdr2:
+            var_offset = 1
+            self.time_col_names = {0: 'Date'}
 
-            for xx, yy in flds.items():
+        if self.verbose:
+            con.print(f'{var_offset=}')
+            con.print(f'time field(s): {list(self.time_col_names.values())}')
+
+        # Parse the field names
+        tmp_flds = [kk.strip() for kk in hdr1.split(self.sep)]
+        tmp_flds.remove('Date')
+
+        flds = {nn+var_offset: hh for nn, hh in enumerate(tmp_flds)}
+
+        for xx, yy in flds.items():
+            if 'seg_outflow' in yy:
                 tfld = yy.split('_')
                 segid = int(tfld[2]) - 1  # Change to zero-based indices
                 poiid = tfld[4]
 
-                poi_flds[xx] = poiid
-                poi_seg_flds[poiid] = segid
+                self.__col_var[xx] = poiid
+                self.__pois.append(poiid)
+                self.__poi_segments[poiid] = segid
+            else:
+                self.__col_var[xx] = yy.strip()
+                self.__basin_vars.append(yy.strip())
 
-        return poi_flds, poi_seg_flds
+    def _read_csv_ascii(self):
+        """Read a PRMS CSV model output file
+        """
 
-    @staticmethod
-    def _read_streamflow_ascii(filename, field_names):
-        """Read the simulated streamflow from a PRMS CSV model output file"""
-        df = pd.read_csv(filename, sep=r'\s+', header=None, skiprows=2, parse_dates={'time': [0, 1, 2]},
-                         index_col='time')
+        poi_idx = {vv: kk for kk, vv in self.__col_var.items()}
+        sel_poi_idx = [poi_idx[kk] for kk in self.variables]
 
-        df.rename(columns=field_names, inplace=True)
+        sel_cols = list(self.time_col_names.keys())
+        sel_cols.extend(sel_poi_idx)
 
-        return df
+        # Read the CSV without the included field names
+        df = pd.read_csv(self.__filename, sep=self.sep, skipinitialspace=True,
+                         header=None, skiprows=2, usecols=sel_cols)
+
+        df.rename(columns=self.time_col_names, inplace=True)
+
+        df.rename(columns=self.__col_var, inplace=True)
+
+        if len(self.time_col_names) > 1:
+            df['time'] = pd.to_datetime(df[self.time_col_names.values()], yearfirst=True)
+            df.drop(columns=self.time_col_names.values(), inplace=True)
+        else:
+            df.rename(columns={self.time_col_names[0]: 'time'}, inplace=True)
+
+        df.set_index('time', inplace=True)
+
+        self.__data = df
